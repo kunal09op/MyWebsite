d23fe04f94b7da6edfec39939f0a39da0e13df76
kunal09op <kunalpatil6525@gmail.com>
2021-10-11 12:33:48 +0530

    third commit

---
+++ js/fun.js
@@ -0,0 +1,10 @@
+$(function(){
+    $("#name_error_message").hide();
+     $("#email_error_message").hide();
+      $("#sub_error_message").hide();
+      var name_error=false;
+      var email_error=false;
+      var sub_error=false;
+
+    
+})
